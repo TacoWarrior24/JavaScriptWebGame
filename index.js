177af9f03d49d125c80879a60d042e0e3d118e1e
TacoWarrior24 <tacowarrior24@gmail.com>
2024-09-27 10:07:26 -0400

    Character movement

--- index.js
+++ index.js
@@ -4,11 +4,34 @@ const ctx = canvas.getContext("2d");
 let x = 100;
 let y = 100;
 let radius = 50;
+let speed = 10;
+
+let downPressed = false;
+let upPressed = false;
+let leftPressed = false;
+let rightPressed = false;
 
 //Game Loop
 function drawGame() {
+    requestAnimationFrame(drawGame);
     clearScreen();
+    inputs();
     drawGreenBlob();
+}
+
+function inputs(){
+    if (downPressed) {
+        y = y + speed;
+    }
+    if (upPressed) {
+        y = y - speed;
+    }
+    if (leftPressed) {
+        x = x - speed;
+    }
+    if (rightPressed) {
+        x = x + speed;
+    }
 }
 
 function drawGreenBlob() {
@@ -23,4 +46,47 @@ function clearScreen() {
     ctx.fillRect(0,0, canvas.width, canvas.height);
 }
 
+document.body.addEventListener('keydown', keyDown)
+document.body.addEventListener('keyup', keyUp)
+
+function keyDown(event){
+    //down
+    if(event.keyCode == 40){
+        downPressed = true;
+    }
+    //up
+    if(event.keyCode == 38){
+        upPressed = true;
+    }
+    //left
+    if(event.keyCode == 37){
+        leftPressed = true;
+    }
+    //right
+    if(event.keyCode == 39){
+        rightPressed = true;
+    }
+}
+
+function keyUp(event){
+    //down
+    if(event.keyCode == 40){
+        downPressed = false;
+    }
+    //up
+    if(event.keyCode == 38){
+        upPressed = false;
+    }
+    //left
+    if(event.keyCode == 37){
+        leftPressed = false;
+    }
+    //right
+    if(event.keyCode == 39){
+        rightPressed = false;
+    }
+}
+
+
 drawGame();
+//setInterval(drawGame,1000/60);
